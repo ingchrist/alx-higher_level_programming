58217a87e18dfb1d6ae37dd905488685e4e68728
root <root@DESKTOP-GP7NKK4.localdomain>
2023-10-30 13:43:33 +0100

    files added

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 wzq = "Holberton"
-wzq_first_3 = word[:3]
-wzq_last_2 = word[-2:]
-middle_wzq = word[1:-1]
+wzq_first_3 = wzq[:3]
+wzq_last_2 = wzq[-2:]
+middle_wzq = wzq[1:-1]
 print("First 3 letters: {}".format(wzq_first_3))
 print("Last 2 letters: {}".format(wzq_last_2))
 print("Middle word: {}".format(middle_wzq))
